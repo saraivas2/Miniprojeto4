d0ccbf916089b0c40ced5560c9372ba798d9f6e7
saraivas2 <saraivas2@yahoo.com.br>
2024-12-16 21:51:17 -0300

    Gravidade
    Para todos os astros

--- Assets/Scripts/Astros/gravidade.cs
+++ Assets/Scripts/Astros/gravidade.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 
 public class gravidade : MonoBehaviour
 {
-    public GameObject Nave;
+    public GameObject Nave1;
+    public GameObject Nave2;
     private float escalaAstro;
     // Start is called before the first frame update
     void Start()
@@ -16,12 +17,20 @@ public class gravidade : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float dist = Vector3.Distance(transform.position,Nave.transform.position);
-        Vector3 distancia = transform.position - Nave.transform.position;
-        float vel = transform.localScale.x / (100*dist);
-        if (dist < escalaAstro)
+        float distN1 = Vector3.Distance(transform.position,Nave1.transform.position);
+        Vector3 distanciaN1 = transform.position - Nave1.transform.position;
+        float velN1 = transform.localScale.x / (100*distN1);
+        if (distN1 < escalaAstro)
         {
-            Nave.transform.Translate(distancia*vel*Time.deltaTime);
+            Nave1.transform.Translate(distanciaN1*velN1*Time.deltaTime);
+        }
+
+        float distN2 = Vector3.Distance(transform.position, Nave2.transform.position);
+        Vector3 distanciaN2 = transform.position - Nave2.transform.position;
+        float velN2 = transform.localScale.x / (100 * distN2);
+        if (distN2 < escalaAstro)
+        {
+            Nave2.transform.Translate(distanciaN2 * velN2 * Time.deltaTime);
         }
     }
 }
